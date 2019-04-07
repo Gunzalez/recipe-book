480818c84280ef943070453c6701aaa3caa9ba80
Segun Konibire <gunzalez@hotmail.com>
2019-04-07 10:00:04 +0100

    shopping list service used

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit } from "@angular/core";
-import { Ingredient } from "../shared/ingredient.model";
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
-  selector: "app-shopping-list",
-  templateUrl: "./shopping-list.component.html",
-  styleUrls: ["./shopping-list.component.sass"]
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.sass']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient("Bread", 8),
-    new Ingredient("Tomatoes", 4)
-  ];
+  ingredients: Ingredient[] = [];
 
-  constructor() {}
+  constructor(private shoppingListService: ShoppingListService) {}
 
-  ngOnInit() {}
-
-  newIngredient(ingredient: Ingredient) {
-    this.ingredients.unshift(ingredient);
+  ngOnInit() {
+    this.ingredients = this.shoppingListService.getIngredients();
   }
+
+  // newIngredient(ingredient: Ingredient) {
+  //   this.ingredients.unshift(ingredient);
+  // }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
 
-  constructor() { }
+  private ingredients: Ingredient[] = [
+    new Ingredient('Bread', 8),
+    new Ingredient('Tomatoes', 4)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
 }
