f6b949a3ea903d0df48e4d463aae9ece79f0909c
Segun Konibire <gunzalez@hotmail.com>
2019-04-29 18:56:12 +0100

    sets up HTML for reactive form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -10,13 +11,29 @@ export class RecipeEditComponent implements OnInit {
 
     id: number;
     editMode = false;
+    recipeForm: FormGroup;
 
-    constructor(private route: ActivatedRoute) { }
+    constructor(private route: ActivatedRoute,
+                private fb: FormBuilder) { }
 
     ngOnInit() {
         this.route.paramMap.subscribe((params: Params) => {
             this.id = +params.get('id');
             this.editMode = params.get('id') !== null;
+            this.formInit();
         });
+    }
+
+    private formInit() {
+        this.recipeForm = this.fb.group({
+            name: ['', [ Validators.required ]],
+            imagePath: ['', [ Validators.required ]],
+            description: ['', [ Validators.required ]]
+        });
+    }
+
+    onSubmit() {
+        console.log(this.recipeForm.value);
+        this.recipeForm.reset();
     }
 }
