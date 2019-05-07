10be8f4c85359f373381664358d62d7035dd0509
Segun Konibire <gunzalez@hotmail.com>
2019-05-07 23:56:06 +0100

    cleans up subscription

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { Recipe } from '../recipe.model';
 
@@ -10,9 +11,10 @@ import { RecipeService } from '../recipe.service';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
 
     recipes: Recipe[] = [];
+    subscription: Subscription;
 
     constructor(private recipeService: RecipeService,
                 private router: Router,
@@ -20,7 +22,7 @@ export class RecipeListComponent implements OnInit {
 
     ngOnInit() {
         this.recipes = this.recipeService.getRecipes();
-        this.recipeService.recipesChanged.subscribe(
+        this.subscription = this.recipeService.recipesChanged.subscribe(
         (recipes: Recipe[]) => {
             this.recipes = recipes;
             }
@@ -30,4 +32,9 @@ export class RecipeListComponent implements OnInit {
     onNewRecipe() {
         this.router.navigate(['new'], { relativeTo: this.route });
     }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
+
 }
