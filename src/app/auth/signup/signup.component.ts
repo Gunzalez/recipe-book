e40c71802b5b4c048eaf265491a2070e2af95f3e
Segun Konibire <gunzalez@hotmail.com>
2019-05-15 20:28:42 +0100

    registration works with firebase

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
+
+@Component({
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.sass']
+})
+export class SignupComponent implements OnInit {
+
+    @ViewChild('form') myForm: NgForm;
+
+    constructor(private authService: AuthService) {}
+
+    ngOnInit() {}
+
+    onSubmit() {
+        this.authService.signUp(this.myForm.value.email, this.myForm.value.password);
+        this.myForm.reset();
+    }
+
+}
