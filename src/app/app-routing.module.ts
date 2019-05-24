bc109900ce465dff550851de667eef5488337192
Segun Konibire <gunzalez@hotmail.com>
2019-05-24 20:50:44 +0100

    Creates AuthModule, encapsulating authentication feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 
 const routes: Routes = [
     {
@@ -13,14 +11,6 @@ const routes: Routes = [
     {
         path: 'shopping-list',
         component: ShoppingListComponent
-    },
-    {
-        path: 'register',
-        component: SignupComponent
-    },
-    {
-        path: 'signin',
-        component: SigninComponent
     },
     {
         path: '**',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { HeaderComponent } from './header/header.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { HttpClientModule } from '@angular/common/http';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
+
 import { RecipesModule } from './recipes/recipes.module';
+import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         ShoppingListComponent,
-        HeaderComponent,
         ShoppingEditComponent,
-        SignupComponent,
-        SigninComponent
+        HeaderComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
         HttpClientModule,
+        AuthModule,
         RecipesModule,
         AppRoutingModule,
         SharedModule
