a06b525d57296d1799a4e592def7a9486d36509c
Segun Konibire <gunzalez@hotmail.com>
2019-05-24 22:28:25 +0100

    Implements lazy loading of recipes module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,25 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
-import { RecipesModule } from './recipes/recipes.module';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-edit/shopping-list.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        HeaderComponent
+        HeaderComponent,
+        HomeComponent
     ],
     imports: [
         BrowserModule,
         HttpClientModule,
         AuthModule,
         ShoppingListModule,
-        RecipesModule,
         AppRoutingModule,
         SharedModule
     ],
